561cc01419b37e5229e454486b16e9ad27665d8f
Victor Suciu <Victor@192.168.0.12>
2018-01-09 20:03:21 -0800

    Changes
    -New GUI layout
    -Added max/min UI

--- Graphing Calculator/src/Window.java
+++ Graphing Calculator/src/Window.java
@@ -11,30 +11,73 @@ public class Window extends JFrame implements ActionListener{
 	JTextField equationIn = new JTextField();
 	JButton graphButton = new JButton("Graph");
 	JButton test = new JButton("test");
-	//test
+	
+	JTextField xMinIn = new JTextField();
+	JTextField xMaxIn = new JTextField();
+	JTextField yMinIn = new JTextField();
+	JTextField yMaxIn = new JTextField();
+	
+	JLabel xMinLab = new JLabel("X Min:");
+	JLabel xMaxLab = new JLabel("X Max:");
+	JLabel yMinLab = new JLabel("Y Min:");
+	JLabel yMaxLab = new JLabel("Y Max:");
+	
+	JButton apply = new JButton("Apply Values");
+	
+	String eq;
+	
 	int width = 600;
 	int height = 600;
 	
-	double xMin = -10.0;
-	double xMax = 10.0;
-	double yMin = -10.0;
-	double yMax = 10.0;
+	double xMin;
+	double xMax;
+	double yMin;
+	double yMax;
 	
 	Equation equation = new Equation();
 	
 	public Window() {
+		xMin = -10.0;
+		xMax = 10.0;
+		yMin = -10.0;
+		yMax = 10.0;
 		System.out.println(equation.getPoints());
 		
 		
-		equationIn.setBounds(width / 2 - 100, height + 10, 160, 30);
-		graphButton.setBounds(width / 2 + 70, height + 10, 70, 30);
+		equationIn.setBounds(width, 10, 160, 30);
+		graphButton.setBounds(width + 45, 40, 70, 30);
 		graphButton.addActionListener(this);
+		
+		xMaxLab.setBounds(width + 35, 120, 45, 30);
+		xMinLab.setBounds(width + 35, xMaxLab.getY() + 40, 45, 30);
+		yMaxLab.setBounds(width + 35, xMinLab.getY() + 40, 45, 30);
+		yMinLab.setBounds(width + 35, yMaxLab.getY() + 40, 45, 30);
+		
+		xMaxIn.setBounds(xMaxLab.getX() + 45, xMaxLab.getY(), 45, 30);
+		xMinIn.setBounds(xMinLab.getX() + 45, xMinLab.getY(), 45, 30);
+		yMaxIn.setBounds(yMaxLab.getX() + 45, yMaxLab.getY(), 45, 30);
+		yMinIn.setBounds(yMaxLab.getX() + 45, yMinLab.getY(), 45, 30);
+		
+		apply.setBounds(width + 20, yMinLab.getY() + 40, 120, 30);
+		apply.addActionListener(this);
 		
 		panel.add(equationIn);
 		panel.add(graphButton);
 		
+		panel.add(xMinLab);
+		panel.add(xMaxLab);
+		panel.add(yMinLab);
+		panel.add(yMaxLab);
+		
+		panel.add(xMinIn);
+		panel.add(xMaxIn);
+		panel.add(yMinIn);
+		panel.add(yMaxIn);
+		
+		panel.add(apply);
+		
 		super.setName("Graphing Calculator");
-		super.setSize(width, height + 100);
+		super.setSize(width + 160, height);
 		super.setContentPane(panel);
 		super.setLayout(null);
 		super.setVisible(true);
@@ -75,10 +118,32 @@ public class Window extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == graphButton) {
-			String eq = equationIn.getText();
+			eq = equationIn.getText();
 			System.out.println("Text = " + eq);
 			equation = new Equation(eq, width, height, xMin, xMax, yMin, yMax);
 			super.repaint();
+		}
+		else if(e.getSource() == apply) {
+			System.out.println("TEST START");
+			if(!xMaxIn.getText().equals("")) {
+				xMax = Double.parseDouble(xMaxIn.getText());
+				System.out.println("xMax");
+			}
+			if(!xMinIn.getText().equals("")) {
+				xMin = Double.parseDouble(xMinIn.getText());
+				System.out.println("xMin");
+			}
+			if(!yMaxIn.getText().equals("")) {
+				yMax = Double.parseDouble(yMaxIn.getText());
+				System.out.println("yMax");
+			}
+			if(!yMinIn.getText().equals("")) {
+				yMin = Double.parseDouble(yMinIn.getText());
+				System.out.println("yMin");
+			}
+			equation = new Equation(eq, width, height, xMin, xMax, yMin, yMax);
+			super.repaint();
+			System.out.println("TEST END");
 		}
 		
 	}
